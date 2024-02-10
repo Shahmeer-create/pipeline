db75f08e2af640f0a53cabc95bf78235b0c2c226
Md. Almas Ali <almaspr3@gmail.com>
2024-02-11 01:58:49 +0600

    fix: allowed host settings.

--- fronty_podcast_demo/settings.py
+++ fronty_podcast_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)v+oy5a86p((-nm!lo%-)=6v)g95^r^d&_#foo^$xj*wmm&jp3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
